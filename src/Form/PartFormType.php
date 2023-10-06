1cf37bce213b5f090d2406cb78e9e5a64b59bb12
hmorejong <hmorejong@gmail.com>
2023-10-06 19:16:17 -0400

    Add style to part views in consistency with the bike template

--- src/Form/PartFormType.php
+++ src/Form/PartFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Part;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,7 @@ class PartFormType extends AbstractType
             ->add('price')
             ->add('color')
             ->add('size')
-            ->add('details');
+            ->add('details',TextareaType::class, ['required' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
